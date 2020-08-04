7f1e3e1785fa8c6271feb272061547041dfaba0d
Simon Holland Flarup <mail@simonflarup.dk>
2020-08-04 15:14:26 +0200

    Version 2.0.0, rewrite

--- src/index.tsx
+++ src/index.tsx
@@ -1,304 +1,120 @@
 "use strict";
 
-import React, { Component, ReactNode } from "react";
-import PropTypes from 'prop-types';
+import React, {ReactNode, FunctionComponent, PropsWithChildren, useState, useRef } from "react";
 import {
-    Dimensions,
-    I18nManager,
     ScrollView,
     StyleSheet,
-    Text,
     TouchableWithoutFeedback,
     View,
-    NativeSyntheticEvent,
-    NativeScrollEvent
+    LayoutChangeEvent
 } from "react-native";
-
-import styles from "./styles";
-
-const { width } = Dimensions.get("window");
 
 type CarouselProps = {
     pageStyle?: object,
     containerStyle?: object,
-    onPageChange?(position: number, currentElement: ReactNode): void,
+    //onPageChange?(position: number, currentElement: ReactNode): void,
 } & Partial<CarouselDefaultProps>;
 
 type CarouselDefaultProps = {
     initialPage?: number | 'center',
-    pageWidth?: number,
     sneak?: number,
-    noItemsText?: string,
-    transitionDelay?: number,
-    currentPage?: number,
-    swipeThreshold?: number
+    vertical?: boolean
 };
 
-type CarouselStates = {
-    gap: number,
-    currentPage: number,
-}
+const Carousel: FunctionComponent<PropsWithChildren<CarouselProps>> = (props) => {
+    const [currentPage, setCurrentPage] = useState(0);
+    const [snapInterval, setSnapInterval] = useState(0);
 
-export default class Carousel extends Component<CarouselProps, CarouselStates> {
+    const [height, setHeight] = useState(0);
+    const [width, setWidth] = useState(0);
 
-    displayName = "Carousel";
-    scrollView: ScrollView | null = null;
+    const scrollview = useRef<ScrollView>(null);
 
-    /*static propTypes = {
-        pageStyle: PropTypes.object,
-        pageWidth: PropTypes.number,
-        children: PropTypes.oneOfType([ PropTypes.arrayOf(PropTypes.node), PropTypes.node ]).isRequired,
-        initialPage: PropTypes.number,
-        containerStyle: PropTypes.object,
-        noItemsText: PropTypes.string,
-        onPageChange: PropTypes.func,
-        sneak: PropTypes.number,
-        transitionDelay: PropTypes.number,
-        currentPage: PropTypes.number,
-        swipeThreshold: PropTypes.number
-    };*/
-
-    static defaultProps = {
-        initialPage: 0,
-        pageWidth: width - 80,
-        sneak: 20,
-        noItemsText: "Sorry, there are currently \n no items available",
-        transitionDelay: 0,
-        currentPage: 0,
-        swipeThreshold: 0.5,
-    } as Partial<CarouselDefaultProps>;
-
-    constructor(props: CarouselProps) {
-        super(props);
-
-        var initialPage: number;
-
-        if (props.initialPage == 'center') {
-            initialPage = Math.floor((this._getPagesCount() - 1) / 2)
+    if (scrollview.current != null) {
+        let position = currentPage * snapInterval;
+        if (props.vertical) {
+            scrollview.current.scrollTo({ x: 0, y: position, animated: true })
         } else {
-            initialPage = props.initialPage as number;
+            scrollview.current.scrollTo({ x: position, y: 0, animated: true })
         }
-
-        this.state = {
-            gap: 0,
-            currentPage: initialPage,
-        };
-
-        //this._scrollTimeout = null;
-
-        this._resetScrollPosition = this._resetScrollPosition.bind(this);
-        this._handleScrollEnd = this._handleScrollEnd.bind(this);
+        console.log("position: " + position + " | CurrentPage: " + currentPage + " | SnapInterval " + snapInterval);
     }
 
-    /*UNSAFE_componentWillMount() {
-        this._calculateGap(this.props);
-    }*/
+    const OnLayout = (event: LayoutChangeEvent) => {
+        const layout = event.nativeEvent.layout;
 
-    componentDidMount() {
-        this._calculateGap(this.props);
-        this._resetScrollPosition(false);
-    }
+        setHeight(layout.height);
+        setWidth(layout.width);
 
-    /*static getDerivedStateFromProps(nextProps, prevState) {
-        return {
-            currentPage: nextProps.currentPage,
-            prevPage: prevState.currentPage,
-        };
-    }*/
-
-    /*UNSAFE_componentWillReceiveProps(nextProps) {
-        
-        this.setState({
-            currentPage: nextProps.currentPage
-        });
-        
-        //this._calculateGap(nextProps);
-    }*/
-
-    componentDidUpdate(prevProps: CarouselProps, prevState: CarouselStates) {
-        /*if (prevProps !== this.props) {
-            this._calculateGap(prevProps);
-        }*/
-        if (prevProps.currentPage !== this.props.currentPage) {
-            this._resetScrollPosition();
-            this._onPageChange(this.props.currentPage!);
-        } else if (prevState.currentPage !== this.state.currentPage) {
-            this._resetScrollPosition();
-            this._onPageChange(this.state.currentPage);
-        }
-    }
-
-    /*componentWillUnmount() {
-        if (this._scrollTimeout) {
-            clearTimeout(this._scrollTimeout);
-        }
-    }*/
-
-    _getPageOffset() {
-        const {
-            pageWidth,
-        } = this.props;
-
-        const {
-            gap,
-        } = this.state;
-
-        return pageWidth! + gap;
-    }
-
-    _getPageScrollX(pageIndex: number) {
-        return pageIndex * this._getPageOffset();
-    }
-
-    _getPagesCount() {
-        return React.Children.count(this.props.children);
-    }
-
-    _resetScrollPosition(animated = true) {
-        // in android, you can't scroll directly in componentDidMount
-        // (http://stackoverflow.com/questions/33208477/react-native-android-scrollview-scrollto-not-working)
-        // however this doesn't work in android for some reason:
-        // InteractionManager.runAfterInteractions(() => {
-        //     this.scrollView.scrollTo({ y: 0, x: pagePosition}, true);
-        //     console.log('scrollView.scrollTo x:', pagePosition);
-        // });
-        // So I was left with an arbitrary timeout.
-        /*if (this._scrollTimeout) {
-            clearTimeout(this._scrollTimeout);
-        }*/
-        /*this._scrollTimeout = setTimeout(() => {
-            this.scrollView.scrollTo({
-                x: this._getPageScrollX(this.state.currentPage),
-                y: 0,
-                animated,
-            });
-            this._scrollTimeout = null;
-        }, this.props.transitionDelay);*/
-        if (this.scrollView) {
-            this.scrollView.scrollTo({
-                x: this._getPageScrollX(this.state.currentPage),
-                y: 0,
-                animated,
-            });
-        }
-    }
-
-    _calculateGap(props: CarouselProps) {
-        const { sneak, pageWidth } = props;
-        if (pageWidth! > width) {
-            throw new Error("invalid pageWidth");
-        }
-        /*
-         ------------
-        |            |
-        |-   ----   -|
-        | | |    | | |
-        | | |    | | |
-        | | |    | | |
-        |-   ----   -|
-        |^-- sneak   |
-        |         ^--- gap
-         ------------
-
-        */
-        const gap = (width - (2 * sneak!) - pageWidth!) / 2;
-        this.setState({ gap: gap });
-    }
-
-    _handleScrollEnd(e: NativeSyntheticEvent<NativeScrollEvent>) {
-        const { swipeThreshold } = this.props;
-        const { currentPage } = this.state;
-
-        const currentPageScrollX = this._getPageScrollX(currentPage);
-        const currentScrollX = e.nativeEvent.contentOffset.x;
-
-        const swiped = (currentScrollX - currentPageScrollX) / this._getPageOffset();
-
-        const pagesSwiped = Math.floor(Math.abs(swiped) + (1 - swipeThreshold!)) * Math.sign(swiped);
-        const newPage = Math.max(Math.min(currentPage + pagesSwiped, this._getPagesCount() - 1), 0)
-
-        if (newPage !== currentPage) {
-            this.setState({ currentPage: newPage });
+        if (props.vertical) {
+            setSnapInterval(layout.height - props.sneak! * 2);
         } else {
-            this._resetScrollPosition();
+            setSnapInterval(layout.width - props.sneak! * 2);
         }
 
-    };
-
-    _onPageChange(position: number) {
-        if (this.props.onPageChange) {
-            var children = this.props.children
-            if (children) {
-                const currentElement = children;
-                this.props.onPageChange(position, currentElement);
-            }
-        }
+        console.log("Height: " + layout.height + " | Width: " + layout.width);
     }
 
-    render() {
-        const { sneak, pageWidth } = this.props;
-        const { gap } = this.state;
-        const computedStyles = StyleSheet.create({
-            scrollView: {
-                paddingLeft: sneak! + gap / 2,
-                paddingRight: sneak! + gap / 2
+    let computedStyles: any = null;
+
+    if (props.vertical) {
+        computedStyles = StyleSheet.create({
+            content: {
+                height: height,
+                width: width,
+                paddingVertical: props.sneak!,
+                marginVertical: -props.sneak!,
             },
-            page: {
-                width: pageWidth,
-                justifyContent: "center",
-                marginLeft: gap / 2,
-                marginRight: gap / 2
+            scrollview: {
+                paddingVertical: props.sneak!,
             }
         });
-
-        // if no children render a no items dummy page without callbacks
-        let body = null;
-        if (!this.props.children) {
-            body = (
-                <TouchableWithoutFeedback>
-                    <View style={[styles.page, computedStyles.page, this.props.pageStyle]}>
-                        <Text style={styles.noItemsText}>
-                            {this.props.noItemsText}
-                        </Text>
-                    </View>
-                </TouchableWithoutFeedback>
-            );
-        }
-        else {
-            const children = Array.isArray(this.props.children) ? this.props.children : [this.props.children]
-            body = children.map((c, index) => {
-                return (
-                    <TouchableWithoutFeedback
-                        key={index}
-                        onPress={() => this.setState({ currentPage: index })}
-                    >
-                        <View
-                            style={[styles.page, computedStyles.page, this.props.pageStyle]}
-                        >
-                            {c}
-                        </View>
-                    </TouchableWithoutFeedback>
-                );
-            });
-        }
-
-        return (
-            <View style={[styles.container, this.props.containerStyle]}>
-                <ScrollView
-                    automaticallyAdjustContentInsets={false}
-                    bounces
-                    contentContainerStyle={[computedStyles.scrollView]}
-                    style={{ flexDirection: (I18nManager && I18nManager.isRTL) ? 'row-reverse' : 'row' }}
-                    decelerationRate={0.9}
-                    horizontal
-                    onScrollEndDrag={this._handleScrollEnd}
-                    ref={c => this.scrollView = c}
-                    showsHorizontalScrollIndicator={false}
-                    onLayout={() => { this._resetScrollPosition(false); }}
-                >
-                    {body}
-                </ScrollView>
-            </View>
-        );
+    } else {
+        computedStyles = StyleSheet.create({
+            content: {
+                height: height,
+                width: width,
+                paddingHorizontal: props.sneak!,
+                marginHorizontal: -props.sneak!,
+            },
+            scrollview: {
+                paddingHorizontal: props.sneak!,
+            }
+        })
     }
+
+
+    let body: any = null;
+    const children = Array.isArray(props.children) ? props.children : [props.children]
+    body = children.map((child, index) => {
+        return (
+            <TouchableWithoutFeedback
+                key={index}
+                onPress={() => { console.log("Setting current page: " + index); setCurrentPage(index); }}
+                style={[{ flex: 1, overflow: 'hidden' }]}
+            >
+                <View
+                    style={[{ flexGrow: 1, overflow: 'hidden' }, computedStyles.content, props.pageStyle]}
+                >
+                    {child}
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    });
+
+    return (
+        <View style={{ flex: 1, }}>
+            <ScrollView bounces={false} showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false} overScrollMode={"never"} contentContainerStyle={[computedStyles.scrollview, props.containerStyle]} onLayout={OnLayout} horizontal={!props.vertical} snapToInterval={snapInterval} ref={scrollview}>
+                {body}
+            </ScrollView>
+        </View>
+    );
 }
+
+Carousel.defaultProps = {
+    initialPage: 0,
+    sneak: 20,
+    vertical: false,
+}
+
+export default Carousel;
